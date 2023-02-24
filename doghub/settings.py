5f48ffa69ca8a1c2ba879a8f7d0dc993c6f290cf
Rafik Saad <rs7983@nyu.edu>
2023-02-23 22:22:37 -0500

    bugfix for crispy_form invalid filter

--- doghub/settings.py
+++ doghub/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
