7273ee15522bb34f1a90f861d0518755d12bb32a
omarkhan5299 <mok236@nyu.edu>
2023-02-23 22:07:39 -0500

    Added first name and last name. Also, after successful reg, now redericts to events

--- doghub_app/forms.py
+++ doghub_app/forms.py
@@ -10,7 +10,7 @@ class NewUserForm(UserCreationForm):
 
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields = ("username","first_name","last_name", "email", "password1", "password2",)
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
--- doghub_app/views.py
+++ doghub_app/views.py
@@ -22,7 +22,7 @@ def register_request(request):
 			user = form.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("home")
+			return redirect("events")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = NewUserForm()
 	return render (request=request, template_name="doghub_app/register.html", context={"register_form":form})
