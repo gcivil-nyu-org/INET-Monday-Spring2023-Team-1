6693824851a1bc2b0588d492815f2281a931ce2f
Rafik Saad <rs7983@nyu.edu>
2023-03-04 22:24:03 -0500

    added user profile and dog profile to models and admin

--- doghub_app/admin.py
+++ doghub_app/admin.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, UserProfile, DogProfile
 
 admin.site.register(CustomUser)
+admin.site.register(UserProfile)
+admin.site.register(DogProfile)
+
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ["email", "username"]
-    
-
