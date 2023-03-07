c966dd7a51610facf57803937e7e0f1d7c005926
omarkhan5299 <mok236@nyu.edu>
2023-03-07 14:12:24 -0500

    fixed black formatting

--- doghub_app/views.py
+++ doghub_app/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
 # from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
