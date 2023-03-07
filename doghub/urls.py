9da4c8a7e6d80ee63cc6ac02c4ab47e34984f1ec
omarkhan5299 <mok236@nyu.edu>
2023-03-07 14:02:42 -0500

    added flake8 to travis

--- doghub/urls.py
+++ doghub/urls.py
@@ -16,4 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("doghub_app.urls"),)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        include("doghub_app.urls"),
+    ),
+]
