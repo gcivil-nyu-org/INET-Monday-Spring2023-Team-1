874d20cc42026a7ff2d4a2aed0fa9e19572e126a
Ashish Chennupati <ashishchennupati@ashishs-air.mynetworksettings.com>
2023-03-19 17:37:17 -0400

    Edit Profile

--- doghub_app/urls.py
+++ doghub_app/urls.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
-from django.urls import path
+#from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -11,4 +11,7 @@ urlpatterns = [
     path("logout", views.logout_request, name="logout"),
     path("dogProfilesCreate", views.dog_profile_create, name="dogProfileCreate"),
     path("user_profile", views.user_profile, name="user_profile"),
+    path("user_profile_edit", views.user_profile_edit, name="user_profile_edit"),
+    path("dog_profile_edit/<int:pk>/", views.dog_profile_edit, name="dog_profile_edit"),
+    #path("dog_profile_delete", views.dog_profile_delete, name="dog_profile_delete"),
 ]
