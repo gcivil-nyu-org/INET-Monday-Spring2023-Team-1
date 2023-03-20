b8bc600b6ffd81313aae0a46ebc0de4a0d8bac21
omarkhan5299 <mok236@nyu.edu>
2023-03-20 13:55:48 -0400

    added tests and increased test coverage to 85%

--- doghub_app/tests.py
+++ doghub_app/tests.py
@@ -1,3 +1,121 @@
-# from django.test import TestCase
+from django.test import TestCase, Client    
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from doghub_app.models import CustomUser, UserProfile, DogProfile
 
-# Create your tests here.
+
+class HomeViewTestCase(TestCase):
+
+    def test_home_view(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Welcome")  # Check if the response contains "DogHub"
+
+class ResgisterViewTestCase(TestCase):
+
+    def setUp(self):
+        self.user_email = "testuser@example.com"
+        self.password = "testpassword"
+
+    def test_register_request_valid(self):
+        # Ensure that a POST request with valid data creates a new user
+        data = {
+            "reg_uemail": self.user_email,
+            "reg_psw": self.password,
+        }
+        response = self.client.post(reverse("register"), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(UserProfile.objects.count(), 0)
+        self.assertEqual(DogProfile.objects.count(), 0)
+
+    def test_register_request_existing_user(self):
+        # Ensure that a POST request with an existing user email displays an error message
+        user = get_user_model().objects.create_user(
+            username=self.user_email, email=self.user_email, password=self.password
+        )
+        data = {
+            "reg_uemail": self.user_email,
+            "reg_psw": self.password,
+        }
+        response = self.client.post(reverse("register"), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "User Exists")
+        self.assertEqual(CustomUser.objects.count(), 1)
+        self.assertEqual(UserProfile.objects.count(), 0)
+        self.assertEqual(DogProfile.objects.count(), 0)
+
+
+
+class RegisterDetailsTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.register_url = reverse('register_details')
+        self.dog_register_url = reverse('dogProfileCreate')
+        self.user_model = get_user_model()
+        self.user = self.user_model.objects.create_user(
+            username='testuser@test.com',
+            email='testuser@test.com',
+            password='testpass123',
+        )
+        self.client.login(email=self.user.email, password='testpass123')
+        self.user_profile_data = {
+            'ufirstname': 'Test',
+            'ulastname': 'User',
+            'uBio': 'Test user bio',
+            'uDOB': '2000-01-01',
+        }
+        self.dog_profile_data = {
+            'dogName': 'Test Dog',
+            'dogBio': 'Test dog bio',
+            'dogDOB': '2010-01-01',
+        }
+
+    def test_register_details_request_success(self):
+        response = self.client.post(self.register_url, data=self.user_profile_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(UserProfile.objects.filter(user_id=self.user).exists())
+        self.assertRedirects(response, reverse('events'))
+
+        response = self.client.post(self.dog_register_url, data=self.dog_profile_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(DogProfile.objects.filter(user_id=self.user).exists())
+        self.assertRedirects(response, reverse('register_details'))
+
+    def test_register_details_request_get(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'doghub_app/register.html')
+
+
+class LoginTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('login')
+        self.user_model = get_user_model()
+        self.user = self.user_model.objects.create_user(
+            username='testuser@test.com',
+            email='testuser@test.com',
+            password='testpass123',
+        )
+
+    def test_login_success(self):
+        self.user_data = {
+            'uemail': 'testuser@test.com',
+            'psw': 'testpass123',
+        }
+        response = self.client.post(self.login_url, data=self.user_data)
+        self.assertEqual(response.status_code,302)
+        self.assertTrue(CustomUser.objects.filter(email=self.user_data['uemail']).exists())
+
+
+    def test_login_failure(self):
+        self.user_data = {
+            'uemail':'none@test.com',
+            'psw':'testpass123',
+        }
+        response = self.client.post(self.login_url, data=self.user_data)
+        self.assertEqual(response.status_code,200)
+        self.assertFalse(CustomUser.objects.filter(email=self.user_data['uemail']).exists())
