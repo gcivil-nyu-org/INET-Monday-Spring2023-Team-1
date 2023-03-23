df0217a72612287a219e22ff8a7d8e755148da00
omarkhan5299 <mok236@nyu.edu>
2023-03-23 14:55:41 -0400

    using env for email credentials

--- doghub/settings.py
+++ doghub/settings.py
@@ -183,6 +183,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "doghub.user@gmail.com"
-EMAIL_HOST_PASSWORD = "bshfgigephlqodtd"
-# APP_PASSWORD = "bshfgigephlqodtd"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
