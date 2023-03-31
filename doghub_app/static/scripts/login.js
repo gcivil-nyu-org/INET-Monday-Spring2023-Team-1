e459239fda2c8065713f9433b2b815e3c275552a
Helen14q7Qi <yq2098@nyu.edu>
2023-03-31 17:52:20 -0400

    fixed pwd validation regirtation

--- doghub_app/static/scripts/login.js
+++ doghub_app/static/scripts/login.js
@@ -1,6 +1,7 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+const errLs = document.getElementById('err_ls');
 
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
@@ -9,3 +10,30 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
+if (errLs!=null){
+  container.classList.add("right-panel-active");
+}
+
+// function checkDOMChange()
+// {
+//     // check for any new element being inserted here,
+//     // or a particular node being modified
+
+//     // call the function again after 100 milliseconds
+//     setTimeout( checkDOMChange, 100 );
+// }
+const observer = new MutationObserver(function(mutations_list) {
+	mutations_list.forEach(function(mutation) {
+		mutation.addedNodes.forEach(function(added_node) {
+			if(added_node.id == 'err_ls') {
+				console.log('#child has been added');
+        container.classList.add("right-panel-active");
+				observer.disconnect();
+			}
+		});
+	});
+});
+
+
+observer.observe(container, { subtree: true, childList: true });
