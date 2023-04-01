bceb988b8514c1a621f0354ed0e9f905521cf171
Helen14q7Qi <yq2098@nyu.edu>
2023-04-01 14:12:55 -0400

    added profile pages for user profile

--- doghub/urls.py
+++ doghub/urls.py
@@ -18,5 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("doghub_app.urls"),),
+    path(
+        "",
+        include("doghub_app.urls"),
+    ),
 ]
